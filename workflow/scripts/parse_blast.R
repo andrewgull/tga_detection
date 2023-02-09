21e6d9f63f2433a3798036e15028d372e9b1c313
andrewgull <andrewgull87@gmail.com>
2023-02-09 13:52:10 +0100

    code reformat

--- workflow/scripts/parse_blast.R
+++ workflow/scripts/parse_blast.R
@@ -1,4 +1,5 @@
 # script to parse blast tables
+# and make histogram of 
 
 # libraries
 library(dplyr)
@@ -76,52 +77,84 @@ if (is.null(opt$unit_len)){
 # READ THE DATA
 
 # a function to process BLAST output
-parse_blast_df <- function(df_name, minimal_length=250, filter_on_positive=TRUE){
-  # read blast results
-  df <- read_delim(df_name, col_names = FALSE, col_select = c(X1, X2, X3, X4, X9, X10))
-  names(df) <- c("query", "subject", "identity", "length", "start.subject", "end.subject")
-  df <- filter(df, length >= minimal_length)
-  # add column indicating that the hit is on positive strain or not
-  df <- df %>% mutate(pos_strand = (end.subject - start.subject) > 0, .keep="all") 
-  # find such positive strand rows that have both FR, and negative strand rows that have both FR
-  if (filter_on_positive){
-    df_strand <- df %>% filter(pos_strand)
-  } else {
-    df_strand <- df %>% filter(!pos_strand)
+parse_blast_df <-
+  function(df_name,
+           minimal_length = 250,
+           filter_on_positive = TRUE) {
+    # read blast results
+    df <-
+      read_delim(df_name,
+                 col_names = FALSE,
+                 col_select = c(X1, X2, X3, X4, X9, X10))
+    names(df) <-
+      c("query",
+        "subject",
+        "identity",
+        "length",
+        "start.subject",
+        "end.subject")
+    df <- filter(df, length >= minimal_length)
+    # add column indicating that the hit is on positive strain or not
+    df <-
+      df %>% mutate(pos_strand = (end.subject - start.subject) > 0,
+                    .keep = "all")
+    # find such positive strand rows that have both FR, 
+    # and negative strand rows that have both FR
+    if (filter_on_positive) {
+      df_strand <- df %>% filter(pos_strand)
+    } else {
+      df_strand <- df %>% filter(!pos_strand)
+    }
+    return(df_strand)
   }
-  return(df_strand)
-}
 
 # a function to join processed BLAST output
-join_filtered_blast <- function(df1_path, df2_path, min_len, on_pos){
-  parsed_list <- lapply(c(df1_path, df2_path), function(x){
-    parse_blast_df(x, minimal_length=min_len, filter_on_positive=on_pos)
+join_filtered_blast <-
+  function(df1_path, df2_path, min_len, on_pos) {
+    parsed_list <- lapply(c(df1_path, df2_path), function(x) {
+      parse_blast_df(x,
+                     minimal_length = min_len,
+                     filter_on_positive = on_pos)
     })
-  df12_joined <- inner_join(parsed_list[[1]], parsed_list[[2]], by="subject")
-  return(df12_joined)
-}
+    df12_joined <-
+      inner_join(parsed_list[[1]], parsed_list[[2]], by = "subject")
+    return(df12_joined)
+  }
 
 # COLLECT POS STRAND ROWS W BOTH FR
-fr12_pos <- join_filtered_blast(opt$input1, opt$input2, min_len=opt$hit_len, on_pos=TRUE)
+fr12_pos <-
+  join_filtered_blast(opt$input1,
+                      opt$input2,
+                      min_len = opt$hit_len,
+                      on_pos = TRUE)
 
 # COLLECT NEG STRAND ROWS W BOTH FR
-fr12_neg <- join_filtered_blast(opt$input1, opt$input2, min_len=opt$hit_len, on_pos=FALSE)
+fr12_neg <-
+  join_filtered_blast(opt$input1,
+                      opt$input2,
+                      min_len = opt$hit_len,
+                      on_pos = FALSE)
 
 # BIND ROWS
 fr12_all <- bind_rows(fr12_pos, fr12_neg)
 
 # FIND CNV
-n_copies <- fr12_all %>% 
+n_copies <- fr12_all %>%
   transmute(n.copies = round(abs(start.subject.y - end.subject.x) / opt$unit_len))
-n_copies_counts <- n_copies %>% group_by(n.copies) %>% summarize(counts=n())
+n_copies_counts <-
+  n_copies %>% group_by(n.copies) %>% summarize(counts = n())
 
 # WRITE THE TABLE W COUNTS
-write.csv(n_copies_counts, opt$output_table, row.names = FALSE, quote = FALSE)
+write.csv(n_copies_counts,
+          opt$output_table,
+          row.names = FALSE,
+          quote = FALSE)
 
 # PLOT
-cnv_histogram <- ggplot(n_copies, aes(n.copies)) + 
-              geom_histogram(binwidth = 0.5, fill="steelblue") + 
-              xlab("AU") +
-              ggtitle("BL-genes copy number distribution", subtitle = paste0("N total spanning reads ", nrow(n_copies)))
+cnv_histogram <- ggplot(n_copies, aes(n.copies)) +
+  geom_histogram(binwidth = 0.5, fill = "steelblue") +
+  xlab("AU") +
+  ggtitle("BL-genes copy number distribution",
+          subtitle = paste0("N total spanning reads ", nrow(n_copies)))
 
-ggsave(plot=cnv_histogram, filename=opt$output_hist)
+ggsave(plot = cnv_histogram, filename = opt$output_hist)
