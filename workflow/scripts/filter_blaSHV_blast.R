79d90e48d3074f55bb2424afa9fad1578d6ae6c8
andrewgull <andrewgull87@gmail.com>
2024-02-15 16:28:18 +0100

    new scripts

---
+++ workflow/scripts/filter_blaSHV_blast.R
@@ -0,0 +1,63 @@
+# script to filter blaSHV blast hits
+
+library(optparse)
+
+# CLI parsing
+option_list <- list(
+  make_option(c("-b", "--blast_bla"),
+              type = "character",
+              default = NULL,
+              help = "blaSHV blasting results",
+              metavar = "file.tsv"),
+  make_option(c("-f", "--blast_fr"),
+              type = "character",
+              default = NULL,
+              help = "joined and filtered table of blast hits of FRs",
+              metavar = "file.tsv"),
+  make_option(c("-e", "--evalue"),
+              type = "double",
+              default = 0.00001,
+              help = "max e-value",
+              metavar = "double"),
+  make_option(c("-o", "--output"),
+              type = "character",
+              default = NULL,
+              help = "filtered blaSHV blast results",
+              metavar = "file.tsv")
+)
+
+opt_parser <- OptionParser(option_list = option_list)
+opt <- parse_args(opt_parser)
+
+if (is.null(opt$blast_bla)) {
+  print_help(opt_parser)
+  stop("blast table of blaSHV must be provided", call. = FALSE)
+}
+if (is.null(opt$blast_fr)) {
+  print_help(opt_parser)
+  stop("Filtered blast table of FR must be provided", call. = FALSE)
+}
+if (is.null(opt$output)) {
+  print_help(opt_parser)
+  stop("Output file must be provided", call. = FALSE)
+}
+
+library(ggplot2)
+suppressPackageStartupMessages(library(dplyr))
+
+fr_df <- readr::read_delim(opt$blast_fr, show_col_types = FALSE)
+bla_df <- read_delim(opt$blast_bla, col_names = FALSE)
+
+bla_df_filt <-
+  bla_df %>%
+  # leave only the reads in filtered FR blast
+  filter(X2 %in% unique(fr_df$subject)) %>%
+  # remove unreliable hits
+  filter(X11 > opt$evalue)
+
+names(bla_df_filt) <- c("query", "subject", "identity", "length", "mismatch",
+                        "gaps", "start.query", "end.query", "start.subject",
+                        "end.subject", "e.value", "bit.score")
+# save results of filtering
+write_delim(bla_df_filt, file = opt$output, delim = "\t")
+print("Fininshed. No erorrs.")
---
+++ workflow/scripts/plot_read_length_and_FR_distances.R
@@ -0,0 +1,64 @@
+# script to plot read length and distance between FRs
+
+library(optparse)
+
+# CLI parsing
+option_list <- list(
+  make_option(c("-b", "--blast"),
+              type = "character",
+              default = NULL,
+              help = "joined and filtered table of blast hits of FRs",
+              metavar = "file.tsv"),
+  make_option(c("-r", "--reads"),
+              type = "character",
+              default = NULL,
+              help = "Nanopore reads in FASTA format",
+              metavar = "reads.fasta"),
+  make_option(c("-o", "--output"),
+              type = "character",
+              default = NULL,
+              help = "output file name (*.png)",
+              metavar = "histogram.png")
+)
+
+opt_parser <- OptionParser(option_list = option_list)
+opt <- parse_args(opt_parser)
+
+if (is.null(opt$blast)) {
+  print_help(opt_parser)
+  stop("Input blast table must be provided", call. = FALSE)
+}
+if (is.null(opt$reads)) {
+  print_help(opt_parser)
+  stop("Input reads file must be provided", call. = FALSE)
+}
+if (is.null(opt$output)) {
+  print_help(opt_parser)
+  stop("Output file must be provided", call. = FALSE)
+}
+
+library(ggplot2)
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(Biostrings))
+
+# read filtered and joined blast
+blast_df <- readr::read_delim(opt$blast, show_col_types = FALSE)
+# read reads
+reads <- readDNAStringSet(opt$reads)
+read_lengths <- width(reads)
+reads_len_df <- tibble("subject" = sub("^(.*?) runid=.*", "\\1", names(reads)),
+                       "read.len" = read_lengths)
+
+# join the two data sets
+blast_df <- left_join(blast_df,
+                      reads_len_df,
+                      by = "subject")
+# make plot
+len_dist_plot <-
+  ggplot(blast_df, aes(distance.btw.FR, read.len)) +
+  geom_point(alpha = 0.3) +
+  xlab("distance between FRs") +
+  ylab("read length")
+
+ggsave(filename = opt$output, plot = len_dist_plot, height = 7, width = 10, units = "in")
+print("Plot was saved to a file. No errors.")
