f1dfcc52c83bec6781cd7f821332bc08668b85a5
andrewgull <andrewgull87@gmail.com>
2024-06-05 11:01:40 +0200

    update get_cn_read_counts.R
    new way of countng reads in CNV bins: separate for reverse and direct hits

--- workflow/scripts/get_cn_read_counts.R
+++ workflow/scripts/get_cn_read_counts.R
@@ -6,7 +6,8 @@
 #  - able to contain 1 blaSHV copy (min length of 5270 bp from the beginning of RED towards RU)
 #  - able to contain 2 bla SHV copies (min len. 8720...)
 # result: a table with CN (0, 1, 2, 3 etc.) and number of reads possibly containing this CN
-# requires dlyr, purrr, tibble
+# requires dlyr, purrr, tibble & Biostrings
+# to count reads with reverse hits, read lengths are required
 
 library(optparse)
 
@@ -16,6 +17,12 @@ option_list <- list(
               type = "character",
               default = NULL,
               help = "a table file with blast results filtered by FR+RU orientation and distance",
+              metavar = "character"),
+  
+  make_option(c("-r", "--reads"),
+              type = "character",
+              default = NULL,
+              help = "reads filtered by FR+RU orientation and distance",
               metavar = "character"),
   
   make_option(c("-c", "--max_cn"),
@@ -50,6 +57,10 @@ if (is.null(opt$table)) {
   print_help(opt_parser)
   stop("Input table file must be provided", call. = FALSE)
 }
+if (is.null(opt$reads)) {
+  print_help(opt_parser)
+  stop("Input reads must be provided", call. = FALSE)
+}
 if (is.null(opt$output)) {
   print_help(opt_parser)
   stop("Output file must be provided", call. = FALSE)
@@ -61,45 +72,100 @@ library(readr)
 library(purrr)
 
 #### Functions ####
-count_reads_with_cn <- function(df, cn = 0, base_len = 1820, increment = 3450) {
-  # df: data frame from RED+RU+Oriented length filtering step
-  # cn: CN of interest
-  # base_len: length of 2*FR+IS
-  # increment: length of incrementation with ewach new copy
+# count reads for each CNV
+count_reads_cn <-
+  function(df,
+           cn = 0,
+           b = 1820,
+           i = 3450,
+           direct = TRUE) {
+    if (direct) {
+      df %>%
+        mutate(keep = end.red > (b + i * cn)) %>%
+        filter(keep) %>%
+        nrow()
+    } else {
+      # reverse: read length must be present!
+      df %>%
+        mutate(keep = (read.len - end.red) > (b + i * cn)) %>%
+        filter(keep) %>%
+        nrow()
+    }
+  }
 
-  # calculate min length
-  min_len <- base_len + increment*cn
-  # filter the input table
-  df_filt <- df %>%
-  mutate(keep = if_else(
-    orient == "direct",
-    end.red > min_len,
-    start.rep.unit > min_len
-  )) %>%
-  filter(keep)
-
-  return(nrow(df_filt))
+# read fasta file and return a table with read ID and length
+# requires Biostrings
+read_length <- function(fasta) {
+  lengths <-
+    Biostrings::width(Biostrings::readDNAStringSet(fasta))
+  
+  len_df <-
+    tibble("subject" = sub(
+      "^(.*?) runid=.*",
+      "\\1",
+      names(Biostrings::readDNAStringSet(fasta))
+    ),
+    "read.len" = lengths)
+  return(len_df)
 }
 
-# read table
-fr_repunit_table <- read.table(opt$table, sep = "\t", header = TRUE)
 
-# make checks
-stopifnot(ncol(fr_repunit_table) == 7)
-stopifnot(nrow(fr_repunit_table) != 0)
+#### Workflow ####
 
-# array of copies
+# read input table
+fr_repunit <- read.table(opt$table, sep = "\t", header = TRUE)
+
+# make table size checks
+stopifnot(ncol(fr_repunit) == 7)
+stopifnot(nrow(fr_repunit) != 0)
+
+# read input reads to get their lengths
+reads_len_df <- read_length(opt$reads)
+
+# separate direct from reverse hits
+fr_repunit_direct <- 
+  fr_repunit %>% 
+  filter(orient == "direct")
+
+# add read lengths to the reverse hits
+fr_repunit_reverse <- 
+  fr_repunit %>% 
+  filter(orient == "reverse") %>% 
+  left_join(reads_len_df, by = "subject")
+
+# create array of copy numbers
 cn_array <- seq(0, opt$max_cn, 1)
-# count the reads for each CNV
-n_reads_w_cn <- map_int(cn_array,
-                        ~ count_reads_with_cn(fr_repunit_table,
-                                              cn = .,
-                                              base_len = opt$base_len,
-                                              increment = opt$increment))
 
-output_table <- data.frame("CN" = cn_array,
-                       "n.reads" = n_reads_w_cn
+# count the reads for each CNV (direct)
+n_reads_cn_dir <- map_int(
+  cn_array,
+  ~ count_reads_cn(
+    fr_repunit_direct,
+    cn = .,
+    b = opt$base_len,
+    i = opt$increment,
+    direct = TRUE
+  )
 )
+
+# count the reads for each CNV (reverse)
+n_reads_cn_rev <- map_int(
+  cn_array,
+  ~ count_reads_cn(
+    fr_repunit_reverse,
+    cn = .,
+    b = opt$base_len,
+    i = opt$increment,
+    direct = FALSE
+  )
+)
+
+# sum the read counts
+n_reads_cn_tot <- n_reads_cn_dir + n_reads_cn_rev
+
+# make a table
+output_table <- data.frame("CN" = cn_array,
+                           "n.reads" = n_reads_cn_tot)
 
 # save results
 write.table(output_table, file = opt$output, sep = "\t", row.names = FALSE, quote = FALSE)
