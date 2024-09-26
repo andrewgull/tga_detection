150a26044c60b0c139a420ff0d8cc5174bebcc42
andrewgull <andrewgull87@gmail.com>
2024-09-26 15:24:05 +0200

    add script for aggregation of frequency tables

---
+++ workflow/scripts/aggregate_frequency_tables.py
@@ -0,0 +1,17 @@
+# script to concatenate tables with frequencies
+import pandas as pd
+import openpyxl
+
+sample_names = snakemake.config['samples']
+table_names = snakemake.input
+output_tsv = snakemake.output['tsv']
+output_xlsx = snakemake.output['xlsx']
+
+dfs = []
+for sample, file in zip(sample_names, table_names):
+    df = pd.read_csv(file, sep='\t')
+    df['sample'] = sample
+    dfs.append(df)
+merged_df = pd.concat(dfs, axis=0)
+merged_df.to_csv(output.tsv, index=False, sep='\t')
+merged_df.to_excel(output.xlsx,index=False, sheet_name='Frequencies')
