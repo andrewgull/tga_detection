d056aad763817fe1f95a2a105621476315dfd1d6
andrewgull <andrewgull87@gmail.com>
2024-11-27 14:53:25 +0100

    refactor: use snakemake object

--- workflow/scripts/filter_fr_hits.R
+++ workflow/scripts/filter_fr_hits.R
@@ -1,69 +1,22 @@
-# script to filter FR blast hits by
-# presence of both RED and GREEN in correct orientation
+############################################################
+# script to filter FR blast hits by presence of
+# both RED and GREEN in correct orientation
+# input: table of RR hits after Red+RU+1820 filtering
+# input: table of GR hits
+# input: max e-value
+# input: minimal hit length
+# input: minimal hit identity
+# output: filtered and joined table of GR+RR+RU+1820 hits
+############################################################
 
-library(optparse)
+#### OPEN LOG ####
+sink(snakemake@log[[1]])
 
-# CLI parsing
-option_list <- list(
-  make_option(c("-r", "--blast_red"),
-              type = "character",
-              default = NULL,
-              help = "a table after Red+RU+1820 filtering",
-              metavar = "character"),
-  
-  make_option(c("-g", "--blast_green"),
-              type = "character",
-              default = NULL,
-              help = "blast hits table for FR2 (green)",
-              metavar = "character"),
-  
-  make_option(c("-o", "--output"),
-              type = "character",
-              default = NULL,
-              help = "filtered and joined table",
-              metavar = "character"),
-  
-  make_option(c("-e", "--evalue"),
-              type = "double",
-              default = 0.00001,
-              help = "max e-value",
-              metavar = "double"),
-  
-  make_option(c("-l", "--min_len"),
-              type = "integer",
-              default = 500,
-              help = "min hit length",
-              metavar = "int"),
-  
-  make_option(c("-i", "--identity"),
-              type = "integer",
-              default = 75,
-              help = "min identity",
-              metavar = "integer")
-)
-
-opt_parser <- OptionParser(option_list = option_list)
-opt <- parse_args(opt_parser)
-
-if (is.null(opt$blast_red)) {
-  print_help(opt_parser)
-  stop("Input file 1 must be provided", call. = FALSE)
-}
-if (is.null(opt$blast_green)) {
-  print_help(opt_parser)
-  stop("Input file 2 must be provided", call. = FALSE)
-}
-if (is.null(opt$output)) {
-  print_help(opt_parser)
-  stop("Output file must be provided", call. = FALSE)
-}
-
-
-#### Libraries ####
+#### LIBRARIES ####
 suppressPackageStartupMessages(library(dplyr))
 library(readr)
 
-#### Functions ####
+#### FUNCTIONS ####
 parse_blast <- function(file_path, region_name) {
   # read blast table
   df <- read_delim(file_path, col_names = FALSE, show_col_types = FALSE)
@@ -112,36 +65,45 @@ filter_red_green <- function(df_red_ru, df_green) {
     filter(!subject %in% reads_multiple_hits)
   df_green_filt <- df_green %>%
     filter(!subject %in% reads_multiple_hits)
-  
   # Filter by orientation
   # Add distance between FRs
   df_joined <- full_join(df_red_ru_filt, df_green_filt, by = "subject") %>%
     filter(!is.na(query.x), !is.na(query.y), orient == orientation) %>%
     mutate(green.red.distance = end.red - start.subject,
-           distance.btw.FR = if_else(green.red.distance < 0, 
-                                     green.red.distance * -1, 
+           distance.btw.FR = if_else(green.red.distance < 0,
+                                     green.red.distance * -1,
                                      green.red.distance * 1))
   return(df_joined)
 }
 
-#### 
+main <- function(red, green, len, evalue, identity) {
+  # add readable query name
+  red$query <- "FR_RU_filt"
+  green_filt <- green %>%
+    # apply basic filtering of blast results
+    filter_blast_p1(min_len = len,
+                    max_e_value = evalue,
+                    min_identity = identity)
+  # check if they are not empty
+  stopifnot(nrow(red) > 0)
+  stopifnot(nrow(green_filt) > 0)
+  # Filtering, part 2
+  blast_joined <- filter_red_green(red, green_filt)
+  return(blast_joined)
+}
 
-#### read and basic filtering if needed
-blast_red <- read_tsv(opt$blast_red, show_col_types = FALSE)
-blast_red$query <- "FR_RU_filt"
-blast_green <- parse_blast(opt$blast_green, "FR_green") %>% 
-  # apply basic filtering of blast results
-  filter_blast_p1(min_len = opt$min_len, 
-                  max_e_value = opt$evalue, 
-                  min_identity = opt$identity)
+#### RUN ####
+blast_red <- read_tsv(snakemake@input[[1]], show_col_types = FALSE)
+blast_green <- parse_blast(snakemake@input[[2]], "FR_green")
+output_filtered <- main(red = blast_red,
+                        green = blast_green,
+                        identity = snakemake@params[[1]],
+                        evalue = snakemake@params[[2]],
+                        len = snakemake@params[[3]])
 
-# check if they are not empty
-stopifnot(nrow(blast_red) > 0)
-stopifnot(nrow(blast_green) > 0)
-
-### Filtering, part 2
-blast_joined <- filter_red_green(blast_red, blast_green)
-
-# Save results
-write_delim(blast_joined, file = opt$output, delim = "\t")
+# write to file
+write_delim(output_filtered, file = snakemake@output[[1]], delim = "\t")
 print("Finished. No erorrs.")
+
+#### CLOSE LOG ####
+sink()
