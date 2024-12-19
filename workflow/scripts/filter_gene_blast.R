3f4a579c9c708c889c0017d20feebc794c7d7311
andrewgull <andrewgull87@gmail.com>
2024-12-19 16:39:26 +0100

    fix: remove old option parsing
    I forgot to remove it

--- workflow/scripts/filter_gene_blast.R
+++ workflow/scripts/filter_gene_blast.R
@@ -6,48 +6,6 @@
 # output: filtered table of blaSHV gene hits
 ##############################################
 
-library(optparse)
-
-# CLI parsing
-option_list <- list(
-  make_option(c("-b", "--blast_bla"),
-              type = "character",
-              default = NULL,
-              help = "blaSHV blasting results",
-              metavar = "file.tsv"),
-  make_option(c("-f", "--blast_fr"),
-              type = "character",
-              default = NULL,
-              help = "a table of RED+RU+filt+GREEN",
-              metavar = "file.tsv"),
-  make_option(c("-e", "--evalue"),
-              type = "double",
-              default = 0.00001,
-              help = "max e-value",
-              metavar = "double"),
-  make_option(c("-o", "--output"),
-              type = "character",
-              default = NULL,
-              help = "filtered blaSHV blast results",
-              metavar = "file.tsv")
-)
-
-opt_parser <- OptionParser(option_list = option_list)
-opt <- parse_args(opt_parser)
-
-if (is.null(opt$blast_bla)) {
-  print_help(opt_parser)
-  stop("blast table of blaSHV must be provided", call. = FALSE)
-}
-if (is.null(opt$blast_fr)) {
-  print_help(opt_parser)
-  stop("Filtered blast table of FR must be provided", call. = FALSE)
-}
-if (is.null(opt$output)) {
-  print_help(opt_parser)
-  stop("Output file must be provided", call. = FALSE)
-}
-
 #### OPEN LOG ####
 sink(snakemake@log[[1]])
 
