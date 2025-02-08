1f4bfa39ae85541fe2e3568bdd70a65b4a8e240f
andrewgull <andrewgull87@gmail.com>
2025-02-08 17:17:53 +0100

    update filter_gene_blast.R
    add error handling, improve comments,
    shorten some functions

--- workflow/scripts/filter_gene_blast.R
+++ workflow/scripts/filter_gene_blast.R
@@ -6,7 +6,7 @@
 # in the previous steps
 # input: blast hits table
 # input: filtered table RED+RU+filt+GREEN
-# input: max e-vlaue
+# input: max e-value
 # output: filtered table of blaSHV gene hits
 ##############################################
 
@@ -17,87 +17,58 @@ sink(snakemake@log[[1]])
 library(ggplot2)
 library(readr)
 library(purrr)
+library(tidyr)
 suppressPackageStartupMessages(library(dplyr))
 
 #### FUNCTIONS ####
-# function to check whether a pair of coords (bla)
-# is within another pair of coords (FRs)
-# returns TRUE if yes, FALSE if no
-is_within <-
-  function(bla_start,
-           bla_end,
-           fr_start,
-           fr_end) {
-    # this is direct strand
-    if (fr_start < fr_end) {
-      # check if both ends of the gene are inside the FR coords
-      if (between(bla_start, fr_start, fr_end) &&
-            between(bla_end, fr_start, fr_end)) {
-        return(TRUE)
-      } else {
-        return(FALSE)
-      }
-      # this is reverse strand
-    } else {
-      # check the same condition but with FR ends swapped
-      if (between(bla_start, fr_end, fr_start) &&
-            between(bla_end, fr_end, fr_start)) {
-        return(TRUE)
-      } else {
-        return(FALSE)
-      }
-    }
+# Function to check whether a pair of coordinates (bla)
+# is within another pair of coordinates (FRs)
+# Returns TRUE if yes, FALSE if no
+is_within <- function(bla_start, bla_end, fr_start, fr_end) {
+  if (fr_start < fr_end) {
+    # Direct strand: check if both ends of the gene
+    # are inside the FR coordinates
+    return(between(bla_start, fr_start, fr_end) &&
+             between(bla_end, fr_start, fr_end))
+  } else {
+    # Reverse strand: check the same condition but with FR ends swapped
+    return(between(bla_start, fr_end, fr_start) &&
+             between(bla_end, fr_end, fr_start))
   }
+}
 
-# this function can be applied to each read id in a table
-# input: read id
-# input: table with FR hits
-# input: table with bla hits
-# return: FALSE if no bla hit is located outside FRs
+# Function to check if any bla hit is located outside FRs for a given read id
+# Returns FALSE if no bla hit is located outside FRs,
 # TRUE if at least one hit is outside FRs
 genes_within_flanks <- function(read_id, fr_df, bla_df) {
   fr_bla_df <- fr_df %>%
     filter(subject == read_id) %>%
-    # this will copy values of FR coords across rows
     left_join(bla_df, by = "subject") %>%
-    # drop na in .y -> no bla hits in this read
     drop_na(start.subject.y) %>%
-    # BLA columns are prefixed by .y
-    # start.subject.x and end.subject.x are the same across all rows now
-    select(
-      subject,
-      start.subject.x,
-      end.subject.x,
-      end.red,
-      start.subject.y,
-      end.subject.y,
-      orientation
-    )
-  # FR coords are always the first elements of end.red, end.subject.x columns
+    select(subject, start.subject.x, end.subject.x,
+           end.red, start.subject.y, end.subject.y, orientation)
+
   answer <- any(
     !map2_lgl(
       fr_bla_df$start.subject.y,
       fr_bla_df$end.subject.y,
-      ~ is_between(.x, .y, fr_bla_df$end.red[1], fr_bla_df$end.subject.x[1])
+      ~ is_within(.x, .y, fr_bla_df$end.red[1], fr_bla_df$end.subject.x[1])
     )
   )
   return(answer)
 }
 
-# function that ties everything together
+# Main function that ties everything together
 main <- function(bla, fr, evalue) {
-  # find reads with bal hits outside FRs
+  # Find reads with bla hits outside FRs
   fr$aberrant <- map_vec(fr$subject, ~ genes_within_flanks(., fr, bla))
 
-  # keep non-aberrant (FALSE) reads only
+  # Keep non-aberrant (FALSE) reads only
   fr <- filter(fr, !aberrant)
 
-  # filter bla hits keeping only those present in FR table
-  bla_filt <-
-    bla %>%
-    # leave only the reads in filtered FR blast
+  # Filter bla hits keeping only those present in FR table
+  bla_filt <- bla %>%
     filter(X2 %in% unique(fr$subject)) %>%
-    # remove unreliable hits
     filter(X11 <= evalue)
 
   names(bla_filt) <- c("query", "subject", "identity", "length", "mismatch",
@@ -107,17 +78,19 @@ main <- function(bla, fr, evalue) {
 }
 
 #### RUN ####
-bla_df <- read_delim(snakemake@input[[1]],
-                     col_names = FALSE,
-                     show_col_types = FALSE)
-fr_df <- read_delim(snakemake@input[[2]],
-                    show_col_types = FALSE)
+tryCatch({
+  bla_df <- read_delim(snakemake@input[[1]], col_names = FALSE,
+                       show_col_types = FALSE)
+  fr_df <- read_delim(snakemake@input[[2]], show_col_types = FALSE)
 
-bla_df_filt <- main(bla = bla_df, fr = fr_df, evalue = snakemake@params[[1]])
+  bla_df_filt <- main(bla = bla_df, fr = fr_df, evalue = snakemake@params[[1]])
 
-# save to file
-write_delim(bla_df_filt, file = snakemake@output[[1]], delim = "\t")
-print("Finished. No erorrs.")
+  # Save to file
+  write_delim(bla_df_filt, file = snakemake@output[[1]], delim = "\t")
+  print("Finished. No errors.")
+}, error = function(e) {
+  print(paste("Error: ", e$message))
+})
 
 #### CLOSE LOG ####
 sink()
