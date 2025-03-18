a23cdb7600a65fbb1f1b748a45743b8fdcf0e9e6
andrewgull <andrewgull87@gmail.com>
2025-03-18 17:00:31 +0100

    style: styleR

--- workflow/scripts/make_bed.R
+++ workflow/scripts/make_bed.R
@@ -30,16 +30,18 @@ is_filtered <- function(filename) {
 read_blast <- function(input_blast) {
   if (is_filtered(input_blast)) {
     blast_bla <- read_tsv(input_blast,
-                          show_col_types = FALSE,
-                          col_names = TRUE)
+      show_col_types = FALSE,
+      col_names = TRUE
+    )
     # check if the correct variant of the blast table was read
     # incorrect version of the table (without headers)
     # will have 'X1' as the first column name
     stopifnot(grepl("query", names(blast_bla)[1]))
   } else {
     blast_bla <- read_tsv(input_blast,
-                          show_col_types = FALSE,
-                          col_names = FALSE)
+      show_col_types = FALSE,
+      col_names = FALSE
+    )
     # check if the correct variant of the blast table was read
     # incorrect version of the table (with headers)
     # will have 'query' and 'blaSHV-1' in X1
@@ -47,10 +49,12 @@ read_blast <- function(input_blast) {
     # check dimensions
     stopifnot(ncol(blast_bla) == 12)
     # set names
-    names(blast_bla) <- c("query", "subject", "identity",
-                          "length", "mismatch", "gaps",
-                          "start.query", "end.query", "start.subject",
-                          "end.subject", "e.value", "bit.score")
+    names(blast_bla) <- c(
+      "query", "subject", "identity",
+      "length", "mismatch", "gaps",
+      "start.query", "end.query", "start.subject",
+      "end.subject", "e.value", "bit.score"
+    )
   }
   # check if the table is empty
   stopifnot(nrow(blast_bla) > 0)
@@ -62,19 +66,25 @@ blast2bed <- function(blast) {
   bla_bed_mix <-
     blast %>%
     select(subject, start.subject, end.subject, query, e.value) %>%
-    mutate(strand = if_else(start.subject < end.subject, "+", "-"),
-           start = start.subject - 1,
-           end = end.subject - 1) %>%
-    rename("chrom" = subject,
-           "name" = query,
-           "score" = e.value) %>%
+    mutate(
+      strand = if_else(start.subject < end.subject, "+", "-"),
+      start = start.subject - 1,
+      end = end.subject - 1
+    ) %>%
+    rename(
+      "chrom" = subject,
+      "name" = query,
+      "score" = e.value
+    ) %>%
     select(chrom, start, end, name, score, strand)
 
   # swap start and end on negative stands and join with positive strands
   bla_bed <- bla_bed_mix %>%
     filter(strand == "-") %>%
-    rename("start" = end,
-           "end" = start) %>%
+    rename(
+      "start" = end,
+      "end" = start
+    ) %>%
     select(chrom, start, end, name, score, strand) %>%
     bind_rows(bla_bed_mix %>% filter(strand == "+"))
 
