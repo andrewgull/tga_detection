6b769ec86758f1a3c8eb09e98f3c1b6272d872dd
Andrei Guliaev <andrei.guliaev@imbim.uu.se>
2025-05-27 16:24:51 +0200

    refactor: use new sample table in aggregate_frequency_tables.py

--- workflow/scripts/aggregate_frequency_tables.py
+++ workflow/scripts/aggregate_frequency_tables.py
@@ -6,13 +6,11 @@ table_names = snakemake.input
 output_tsv = snakemake.output['tsv']
 output_xlsx = snakemake.output['xlsx']
 
-# parse the sample names from config and sample list
-# the first and only column of this df
-# will contain the sample names
-samples = pd.read_csv(snakemake.config["samples"], dtype={"samples": str})
+# parse the sample names table config
+samples = pd.read_csv(snakemake.config["sample_table"], sep="\t", dtype={"sample": str, "path": str})
 
 dfs = []
-for sample, file in zip(samples["samples"], table_names):
+for sample, file in zip(samples["sample"], table_names):
     df = pd.read_csv(file, sep='\t')
     df['sample'] = sample
     dfs.append(df)
