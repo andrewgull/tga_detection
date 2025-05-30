2238ea7b1e47055f4f10b0f08aa5499fc34d60f6
Andrei Guliaev <andrei.guliaev@imbim.uu.se>
2025-05-30 16:57:59 +0200

    feat: add tsv2dict() to read parameters to dictionary

--- workflow/scripts/utils.py
+++ workflow/scripts/utils.py
@@ -16,3 +16,20 @@ def get_sample_path(wildcards: Any, data_frame: pd.DataFrame) -> str:
         return row["path"].values[0]
     else:
         raise ValueError(f"Sample '{wildcards.sample}' not found in the provided DataFrame.")
+
+
+def tsv2dict(file_path) -> dict:
+    """
+    Read a TSV file and convert it to a dictionary.
+    arguments:
+        file_path: Path to the TSV file.
+    returns:
+        dict: A dictionary with parameters as keys and their values.
+    """
+    df = pd.read_csv(
+        file_path,
+        sep="\t",
+        dtype={"param": str, "value": str}
+    )
+    data_dict = df.set_index("param")["value"].to_dict()
+    return data_dict
